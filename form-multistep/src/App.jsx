2a50441e9eba4247ba09d462cb6469dba4cb27dd
Eduardo Antonino <eduardoguilhermedem987@gmail.com>
2026-05-22 17:03:06 -0300

    criação das Steps dinâmicas

--- form-multistep/src/App.jsx
+++ form-multistep/src/App.jsx
@@ -4,11 +4,13 @@ import {FiSend} from "react-icons/fi"
 import UserForm from './components/UserForm'
 import ReviewForm from "./components/ReviewForm"
 import Thanks from "./components/Thanks"
+import Steps from "./components/Steps"
 
 // Hooks
 import { useForm } from "./hooks/useForm"
 
 import './App.css'
+
 
 function App() {
 
@@ -30,6 +32,7 @@ function App() {
           <p>Ficamos feliz com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
         </div>
         <div className="form-container">
+          <Steps currentStep={currentStep}/>
           <form onSubmit={(e) => e.preventDefault()}> 
             <div className="inputs-container">{currentComponent}</div>
             <div className="actions">
