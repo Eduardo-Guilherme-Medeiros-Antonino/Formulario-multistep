267c0f8819d31b540d789d268b49bbe1092fcb4f
Eduardo Antonino <eduardoguilhermedem987@gmail.com>
2026-05-22 15:38:52 -0300

    Criando hook de troca de inputs

--- form-multistep/src/App.jsx
+++ form-multistep/src/App.jsx
@@ -1,9 +1,25 @@
 // Componentes
 import {GrFormNext, GrFormPrevious} from "react-icons/gr"
+import UserForm from './components/UserForm'
+import ReviewForm from "./components/ReviewForm"
+import Thanks from "./components/Thanks"
+
+// Hooks
+import { useForm } from "./hooks/useForm"
 
 import './App.css'
 
 function App() {
+
+  const formComponents = [ 
+    <UserForm/>,
+    <ReviewForm/>,
+    <Thanks/>
+  ]
+
+  const {currentStep, currentComponent} = useForm(formComponents)
+
+
 
   return (
     <>
@@ -11,12 +27,11 @@ function App() {
         <div className="header">
           <h2>Deixe sua avaliação</h2>
           <p>Ficamos feliz com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
-
         </div>
         <div className="form-container">
           <p>etapas</p>
           <form>
-            <div className="inputs-container"></div>
+            <div className="inputs-container">{currentComponent}</div>
             <div className="actions">
               <button type='button'>
                 <GrFormPrevious/>
